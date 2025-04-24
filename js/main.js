ee923d17b1f89df0632f77bc262449fe5b33fa13
kassaab <abrhamkassa12@gmail.com>
2025-04-23 21:53:16 -0700

    complete

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 //alert('test');
 
 //https://tinyurl.com/dynamic-html-checker
-document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+document.getElementById("html-checker").setAttribute("href", "https://validator.w3.org/nu/?doc=" + location.href);
 
-document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);  
+document.getElementById("css-checker").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
 
 //manages mobile nav 
 function myFunction() {
@@ -13,4 +13,4 @@ function myFunction() {
     } else {
         x.className = "topnav";
     }
-}  
+}
